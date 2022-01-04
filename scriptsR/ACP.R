4876c21eba4314d20cf282029e04ebec4bf57baf
Steven Neira <stevencali9113@gmail.com>
2022-01-03 20:03:42 -0500

    force the figure after the text

--- scriptsR/ACP.R
+++ scriptsR/ACP.R
@@ -7,9 +7,11 @@ transactions <- read.csv(file = "/Users/steven/Documents/cnam/methodes_descripti
 transactions <- distinct(transactions, fullVisitorId, .keep_all= TRUE)
 
 variables_ACP <- select(transactions,hits, 
-                                  totalTransactionRevenue,
-                                  browser_width, browser_height, 
-                                  itemCount, browser_name, deviceCategory,paymentMethod)
+                        pageviews, 
+                        totalTransactionRevenue, 
+                        transactions, timeOnSite, 
+                        browser_width, browser_height, 
+                        itemCount, browser_name)
 
 
-res <- Factoshiny(variables_ACP)
+res <- Factoshiny(transactions)
--- scriptsR/BiVarie.R
+++ scriptsR/BiVarie.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(stargazer)
-library(Rcmdr)
+#library(Rcmdr)
 library(car)
 library(lattice)
 library(RColorBrewer)
@@ -8,6 +8,7 @@ library(RColorBrewer)
 transactions <- read.csv(file = "/Users/steven/Documents/cnam/methodes_descriptives/scriptsR/datasetClarinsTransactions.csv")
 transactions <- distinct(transactions, fullVisitorId, .keep_all= TRUE)
 options(scipen = 100)
+transactions <- transactions %>% filter(timeOnSite > 100)
 
 variables_quantitatives <- select(transactions, hits, pageviews, 
                                   totalTransactionRevenue, 
@@ -26,11 +27,17 @@ mat.cor<-cor(variables_quantitatives)
 print(round(mat.cor,2))
 
 
+coul <- brewer.pal(5, "Set2")
 
-Boxplot(timeOnSite~deviceCategory, data = transactions)
+tmp <- Boxplot(totalTransactionRevenue~browser_name, data = transactions, axes=FALSE)
+text(seq_along(transactions$browser_name), par("usr")[3] - 0.5, labels = names(transactions$browser_name), srt = 90, adj = 1, xpd = TRUE);
 
-histogram( ~itemCount | deviceCategory, data = transactions,type="density"
-           ,col="lightgreen"
-           ,ylab="",
+histogram( ~totalTransactionRevenue | browser_name, data = transactions,
+           col="lightgreen",
+           ylab="",
            nint=12)
+
+variables_quantitatives$timeOnSite
+
+
 
